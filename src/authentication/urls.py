3478593c9618d265b9d21f6fdf0ea51690785f0c
Masibonge Masinga <masi.masinga@resolute.education>
2024-03-15 15:20:00 +0200

    feat - add reset and forgot password api

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, CustomTokenObtainPairView
+from .views import RegisterView, LoginView, LogoutView, CustomTokenObtainPairView, PasswordResetView, SetPasswordView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-]
+    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password/reset/confirm/<uidb64>/<token>/', SetPasswordView.as_view(), name='password_reset_confirm'),
+ ]
